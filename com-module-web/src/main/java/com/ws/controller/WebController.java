af3b7b3a1e9067ad131e41f9034c8df92f6929b1
SHUO12138 <wangshyo357159@qq.com>
2019-01-15 11:43:55 +0800

    注释掉了一些东西

--- com-module-web/src/main/java/com/ws/controller/WebController.java
+++ com-module-web/src/main/java/com/ws/controller/WebController.java
@@ -7,12 +7,12 @@ public class WebController {
     public static void main(String[] args) {
         Service service = new Service();
         Service.getService("web");
-        while (true) {
-            for (int i = 999999999; i > 0; i--) {
-                for (int j = 0; j < 99999999; j++) {
-                    System.out.println(i * j * i * j);
-                }
-            }
-        }
+//        while (true) {
+//            for (int i = 999999999; i > 0; i--) {
+//                for (int j = 0; j < 99999999; j++) {
+//                    System.out.println(i * j * i * j);
+//                }
+//            }
+//        }
     }
 }
